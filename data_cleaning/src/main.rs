bca4d8564419fdc3b00b1ae76818372c3c55bf93
Artur <donaldson@pre-sustainability.com>
2023-09-29 09:13:40 +0200

    Read CSV using datafusion

---
+++ data_cleaning/src/main.rs
@@ -0,0 +1,17 @@
+use tokio;
+use datafusion::prelude::*;
+
+#[tokio::main]
+async fn main() -> datafusion::error::Result<()>{
+    let csv_path = "example_csv.csv";
+    let n = 10;
+
+    let ctx = SessionContext::new();
+    ctx.register_csv("csv_to_read", &csv_path,CsvReadOptions::new()).await?;
+    let sql_query = format!("SELECT * FROM csv_to_read LIMIT {n}");
+    let df = ctx.sql(&(sql_query)).await?;
+
+    df.show().await?;
+    
+    Ok(())
+}
